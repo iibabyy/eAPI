e12b01eea975563ff29a83706dd8a5142a708568
ibaby <utileidrissa@gmail.com>
2025-02-07 03:29:16 +0100

    added products number in stock column

--- backend/requirements/migrations/20250110175352_product_table.up.sql
+++ backend/requirements/migrations/20250110175352_product_table.up.sql
@@ -6,12 +6,28 @@ CREATE TABLE IF NOT EXISTS products (
 	user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 	description VARCHAR(1000),
 	price_in_cents BIGINT NOT NULL CHECK(price_in_cents >= 0),
+	number_in_stock INTEGER NOT NULL,
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 
---	triggers
+
+	CREATE OR REPLACE FUNCTION initial_number_in_stock() 
+	RETURNS TRIGGER AS $$
+	BEGIN
+		-- Vérifie si l'utilisateur essaie de commander son propre produit
+		IF NEW .number_in_stock < 1
+			THEN RAISE EXCEPTION 'positive_initial_number_in_stock';
+		END IF;
+		RETURN NEW;
+	END;
+	$$ LANGUAGE plpgsql;
+	--  --
+	CREATE TRIGGER check_initial_number_in_stock
+	BEFORE INSERT OR UPDATE ON products
+	FOR EACH ROW
+	EXECUTE FUNCTION initial_number_in_stock();
 
 --	--	update timestamp	
 
--- backend/requirements/migrations/20250110223425_orders_table.up.sql
+++ backend/requirements/migrations/20250110223425_orders_table.up.sql
@@ -4,15 +4,41 @@ CREATE TABLE IF NOT EXISTS orders (
 	id UUID NOT NULL PRIMARY KEY DEFAULT (uuid_generate_v4()),
 	user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 	product_id UUID NOT NULL REFERENCES products(id),
-	products_number INTEGER NOT NULL CHECK(products_number >= 1),
+	products_number INTEGER NOT NULL CHECK(
+		products_number >= 1 AND
+		products_number <= 1000
+	),
 	order_details_id UUID UNIQUE REFERENCES users(id) ON DELETE SET NULL,
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
---	triggers
+--	function/triggers
 
---	--	update timestamp	
+	--	--	check user != product.user
+
+	CREATE OR REPLACE FUNCTION check_if_product_belong_to_buyer() 
+	RETURNS TRIGGER AS $$
+	BEGIN
+		-- Vérifie si l'utilisateur essaie de commander son propre produit
+		IF EXISTS (
+			SELECT 1
+			FROM product
+			WHERE id = NEW.product_id AND user_id = NEW.user_id
+		) THEN
+			RAISE EXCEPTION 'auto-buying';
+		END IF;
+		RETURN NEW;
+	END;
+	$$ LANGUAGE plpgsql;
+	--  --
+	CREATE TRIGGER check_user_product
+	BEFORE INSERT OR UPDATE ON orders
+	FOR EACH ROW
+	EXECUTE FUNCTION check_if_product_belong_to_buyer();
+
+
+	--	--	update timestamp	
 
 	CREATE TRIGGER update_order_timestamp
 	BEFORE UPDATE ON orders
