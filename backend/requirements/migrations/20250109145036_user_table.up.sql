046da3f5caaff36c97bd98b0c7f5a2f5411dde4b
ibaby <utileidrissa@gmail.com>
2025-02-08 15:34:23 +0100

    added 'active-token' column to users table

--- backend/requirements/migrations/20250109145036_user_table.up.sql
+++ backend/requirements/migrations/20250109145036_user_table.up.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
 	name VARCHAR(100) NOT NULL CHECK(name <> ''),
 	email VARCHAR(255) NOT NULL CHECK(email <> '') UNIQUE,
 	password VARCHAR(255) NOT NULL CHECK(password <> ''),
+	active_token VARCHAR(255) DEFAULT NULL,
 	sold_in_cents BIGINT NOT NULL DEFAULT 0 CHECK(sold_in_cents >= 0),
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
