bb8dab858014fa1b5450e94942a2794a68198103
ibaby <utileidrissa@gmail.com>
2025-02-12 14:58:39 +0100

    added photo_url column to users table

--- migrations/20250109145036_user_table.up.sql
+++ migrations/20250109145036_user_table.up.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
 	email VARCHAR(255) NOT NULL CHECK(email <> '') UNIQUE,
 	password VARCHAR(255) NOT NULL CHECK(password <> ''),
 	last_token_id VARCHAR(255) DEFAULT NULL,
+	photo_url VARCHAR(255) DEFAULT NULL,
 	sold_in_cents BIGINT NOT NULL DEFAULT 0 CHECK(sold_in_cents >= 0),
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
